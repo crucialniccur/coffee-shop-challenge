1bb24d4cc3c06bd04bcf8108df00a17023257c7d
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 12:12:05 +0300

    add validation for the name in Coffee

--- coffee.py
+++ coffee.py
@@ -1,3 +1,7 @@
 class Coffee:
     def __init__(self, name):
-        self._name = name
+        if isinstance(name, str) and len(name) >= 3:
+            self._name = name
+        else:
+            raise ValueError(
+                'name should be a string and greater than or equal to 3 chars ')
