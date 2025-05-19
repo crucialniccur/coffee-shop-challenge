aca37639e3ec6bf12a2a1ed78e2ac2e4dd16bc0e
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 12:18:26 +0300

    define the Order class with the init taking in a customer, coffee and a price

--- order.py
+++ order.py
@@ -1,2 +1,9 @@
 from customer import Customer
 from coffee import Coffee
+
+
+class Order:
+    def __init__(self, customer, coffee, price):
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
