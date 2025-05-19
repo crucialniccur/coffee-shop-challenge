17b5841e4aa50dde7a890ce8350a372f522136d2
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 12:19:50 +0300

    add validation of the customer

--- order.py
+++ order.py
@@ -4,6 +4,8 @@ from coffee import Coffee
 
 class Order:
     def __init__(self, customer, coffee, price):
+        if not isinstance(customer, Customer):
+            raise TypeError('must be a Customer instance!')
         self._customer = customer
         self._coffee = coffee
         self._price = price
