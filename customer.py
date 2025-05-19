0516ee7d4c13f49ee022e03bdbf50329f5566228
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 12:06:49 +0300

    define the property and setter methods for the customer name

--- customer.py
+++ customer.py
@@ -1,3 +1,12 @@
 class Customer:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # add validation with isinstance later
+        self._name = value
