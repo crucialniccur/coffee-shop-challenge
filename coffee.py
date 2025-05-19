823a75dc8ab83bca268b3ee982cf5d0e47011d4a
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 12:35:47 +0300

    add orders and customers in coffee

--- coffee.py
+++ coffee.py
@@ -12,3 +12,9 @@ class Coffee:
     @property
     def name(self):
         return self._name
+
+    def orders(self):
+        return [order for order in Order.all() if order.coffee == self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
