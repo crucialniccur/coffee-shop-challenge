f6e360acad7499b5c846b88bbcab659cb549d7f5
Mati Joseph <crucialniccur@gmail.com>
2025-05-19 13:51:56 +0300

    fix typo

--- customer.py
+++ customer.py
@@ -18,7 +18,7 @@ class Customer:
         return [order for order in Order.all() if order.customer == self]
 
     def coffees(self):
-        return list({order.coffe for order in self.orders()})
+        return list({order.coffee for order in self.orders()})
 
     def create_order(self, coffee, price):
         return Order(self, coffee, price)
