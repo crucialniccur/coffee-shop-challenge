0e1c3994bf6ca30a44f8205171c34c60730ca877
Mati Joseph <crucialniccur@gmail.com>
2025-05-27 07:42:17 +0300

    move import of the Order into the functions where they are needed

--- customer.py
+++ customer.py
@@ -1,5 +1,3 @@
-from order import Order
-
 
 class Customer:
     def __init__(self, name):
@@ -18,10 +16,13 @@ class Customer:
         self._name = value
 
     def orders(self):
+        from order import Order
         return [order for order in Order.all() if order.customer == self]
 
     def coffees(self):
+        from order import Order
         return list({order.coffee for order in self.orders()})
 
     def create_order(self, coffee, price):
+        from order import Order
         return Order(self, coffee, price)
